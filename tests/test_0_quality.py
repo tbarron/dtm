edef1cf56c9297c924ba9a14ae8b1e212a19258a
Tom Barron <tusculum@gmail.com>
2019-04-19 09:21:02 -0400

    Run flake8 on the payload code

--- tests/test_0_quality.py
+++ tests/test_0_quality.py
@@ -10,6 +10,6 @@ def test_flake():
     """
     pytest.dbgfunc()
     globble = sorted(glob.glob("tests/test*.py"))
-    cmd = "flake8 bcal.py dt.py bear/__init__.py {}".format(" ".join(globble))
+    cmd = "flake8 dtm/__init__.py {}".format(" ".join(globble))
     result = tbx.run(cmd)
     assert result == ""
