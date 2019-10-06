c8368210ced61617401899fbc3baea4c89c33752
Tom Barron <tusculum@gmail.com>
2019-10-06 15:19:39 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Embellished datetime objects',
+    'author': 'Tom Barron',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'tom.barron@comcast.net',
+    'version': '0.1',
+    # 'install_requires': ['git://github.com/tbarron/tbx.git#egg=tbx'],
+    'packages': ['dtm'],
+    'scripts': [],
+    'name': 'dtm'
+}
+
+setup(**config)
