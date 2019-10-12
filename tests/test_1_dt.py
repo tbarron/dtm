3ee6949fb055ca7cd40f9a0209ff21c673e4f59c
Tom Barron <tusculum@gmail.com>
2019-10-12 13:06:46 -0400

    Whitespace cleanup

--- tests/test_1_dt.py
+++ tests/test_1_dt.py
@@ -352,13 +352,12 @@ def test_deployable():
     assert untracked == [], "You have untracked files"
     assert changed == [], "You have unstaged updates"
     assert staged == [], "You have updates staged but not committed"
-    
+
     if tbx.git_current_branch() != 'master':
         return True
-    
+
     last_tag = tbx.git_last_tag()
     msg = "Version ({}) does not match tag ({})".format(tbx.version(),
                                                         last_tag)
     assert tbx.version() == last_tag, msg
     assert tbx.git_hash() == tbx.git_hash(last_tag), "Tag != HEAD"
-                                                                        
