729ab97ffd1eb3a8d01d26141b68a71a433aedb5
Tom Barron <tusculum@gmail.com>
2019-10-12 13:03:45 -0400

    Add test_deployable, copied from project tbx

--- tests/test_1_dt.py
+++ tests/test_1_dt.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from dtm import dt, dt_error, version
 import pytest
+import tbx
 
 
 # -----------------------------------------------------------------------------
@@ -336,3 +337,28 @@ def test_ymd(when):
     """
     pytest.dbgfunc()
     assert dt(when).ymd() == when
+
+
+# -----------------------------------------------------------------------------
+def test_deployable():
+    """
+    Check that 1) no untracked files are hanging out, 2) no staged but
+    uncommitted updates are outstanding, 3) no unstaged, uncommitted changes
+    are outstanding, 4) the most recent git tag matches HEAD, and 5) the most
+    recent git tag matches the current version.
+    """
+    pytest.dbgfunc()
+    staged, changed, untracked = tbx.git_status()
+    assert untracked == [], "You have untracked files"
+    assert changed == [], "You have unstaged updates"
+    assert staged == [], "You have updates staged but not committed"
+    
+    if tbx.git_current_branch() != 'master':
+        return True
+    
+    last_tag = tbx.git_last_tag()
+    msg = "Version ({}) does not match tag ({})".format(tbx.version(),
+                                                        last_tag)
+    assert tbx.version() == last_tag, msg
+    assert tbx.git_hash() == tbx.git_hash(last_tag), "Tag != HEAD"
+                                                                        
