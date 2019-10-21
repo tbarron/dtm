b0b0c9e4f2ce39df8eb0ec5a58e1f9cbb2959b83
Tom Barron <tusculum@gmail.com>
2019-10-21 15:59:47 -0400

    Add test_function_doc to catch functions missing a non-empty __doc__

--- tests/test_0_quality.py
+++ tests/test_0_quality.py
@@ -1,6 +1,8 @@
 import glob
+import inspect
 import pytest
 import tbx
+from importlib import import_module
 
 
 # -----------------------------------------------------------------------------
@@ -9,7 +11,49 @@ def test_flake():
     Check code quality
     """
     pytest.dbgfunc()
-    globble = sorted(glob.glob("tests/test*.py"))
-    cmd = "flake8 dtm/__init__.py {}".format(" ".join(globble))
+    globble = sorted(glob.glob("dtm/*.py"))
+    globble.extend(sorted(glob.glob("tests/*.py")))
+    cmd = "flake8 {}".format(" ".join(globble))
     result = tbx.run(cmd)
     assert result == ""
+
+
+# -----------------------------------------------------------------------------
+def test_function_doc():
+    """
+    Verify that each of our functions have a __doc__ string
+    """
+    pytest.dbgfunc()
+
+    importables = ['dtm', 'dtm.__main__']
+    importables.extend([tbx.basename(_).replace('.py', '')
+                        for _ in glob.glob('tests/*.py')])
+
+    missing_doc = []
+    for mname in importables:
+        mod = import_module(mname)
+        for name, obj in inspect.getmembers(mod, inspect.isclass):
+            if doc_missing(obj) and name not in missing_doc:
+                missing_doc.append(name)
+
+            for mthname, mthobj in inspect.getmembers(obj, inspect.isfunction):
+                if doc_missing(mthobj) and mthname not in missing_doc:
+                    missing_doc.append("{}.{}".format(name, mthname))
+
+        for name, obj in inspect.getmembers(mod, inspect.isfunction):
+            if doc_missing(obj) and name not in missing_doc:
+                missing_doc.append("{}.{}".format(mname, name))
+
+    if missing_doc:
+        pytest.fail("Items missing __doc__:\n   " + "\n   ".join(missing_doc))
+
+
+# -----------------------------------------------------------------------------
+def doc_missing(obj):
+    """
+    Check *obj* for a non-empty __doc__ element
+    """
+    if not hasattr(obj, '__doc__') or obj.__doc__ is None:
+        return True
+    else:
+        return False
