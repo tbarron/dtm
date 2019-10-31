79e2f70dd8c528f8c8ad0cabfce9e5a97ef82d16
Tom Barron <tusculum@gmail.com>
2019-10-31 18:33:34 -0400

    Utility function to generate an unsupported operand type message...
    ... wrapped in a TypeError exception

--- tests/dtm_test_utils.py
+++ tests/dtm_test_utils.py
@@ -8,3 +8,12 @@ tz_utc = pytz.timezone('utc')
 tz_csdt = pytz.timezone('cst6cdt')
 tz_esdt = pytz.timezone('est5edt')
 tz_local = tzlocal.get_localzone()
+
+
+# -----------------------------------------------------------------------------
+def unsupp(opd, left, right):
+    """
+    Format an unsupported operand type message wrapped in a TypeError exception
+    """
+    msg = "unsupported operand type(s) for {}: '{}' and '{}'"
+    return TypeError(msg.format(opd, left, right))
