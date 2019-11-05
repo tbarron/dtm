21b724f3c7c2eb845fc210fdcba131fe03171d96
Tom Barron <tusculum@gmail.com>
2019-11-05 05:31:56 -0500

    Generalized comparison operation test functions

--- tests/dtm_test_utils.py
+++ tests/dtm_test_utils.py
@@ -24,5 +24,37 @@ def unsupp_a(opd, left, right):
     """
     Format an unsupported operand type message wrapped in a TypeError exception
     """
-    msg = "unsupported operand type(s) for {}: <{}> and <{}>"
+    msg = "unsupported operand type(s) for {}:  <{}> and <{}>"
     return TypeError(msg.format(opd, left, right))
+
+
+# -----------------------------------------------------------------------------
+def operate(op, left, right):
+    """
+    Apply the indicated operator to support generality in cmp_exception()
+    """
+    if op == '==':
+        return left == right
+    elif op == '!=':
+        return left != right
+    elif op == '<':
+        return left < right
+    elif op == '<=':
+        return left <= right
+    elif op == '>':
+        return left > right
+    elif op == '>=':
+        return left >= right
+
+
+# -----------------------------------------------------------------------------
+def cmp_exception(op, left, right, exp):
+    """
+    Generalize tests for comparison operators with exception handling
+    """
+    if isinstance(exp, Exception):
+        with pytest.raises(type(exp)) as err:
+            operate(op, left, right)
+        assert str(exp) in str(err.value)
+    else:
+        assert operate(op, left, right) is exp
